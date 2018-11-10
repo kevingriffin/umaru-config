31b887db73923392ecd333614a49a1d07cc39379
Kevin Griffin <me@kevin.jp>
2018-11-10 14:00:59 +0000

    Add letsencrypt renewal

--- tomoyo/local.nix
+++ tomoyo/local.nix
@@ -6,6 +6,16 @@
     ../eikaiwa-servers.nix
    ];
 
+  security.acme.certs."kevin.jp" = {
+    webroot = "/var/www/challenges";
+    email   = "me@kevin.jp";
+  };
+
+  security.acme.certs."dev.kevin.jp" = {
+    webroot = "/var/www/challenges";
+    email   = "me@kevin.jp";
+  };
+
   boot.loader.grub.device = "/dev/vda";
   networking.hostName = "tomoyo";
   nix.buildCores = 2;
