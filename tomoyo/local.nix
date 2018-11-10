fd417035c67e38b0eca905f7c8144f13d3e83b6d
Kevin Griffin <me@kevin.jp>
2018-11-10 08:56:58 +0000

    Correct settings for Tomoyo.

--- tomoyo/local.nix
+++ tomoyo/local.nix
@@ -1,21 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = false;
-
-  boot.initrd.luks.devices = [
-   {
-     name = "root";
-     device = "/dev/disk/by-uuid/72bba5e8-fc90-440f-b7a0-ef3d989ad7ad";
-     preLVM = true;
-     allowDiscards = true;
-   }
-  ];
-
-  hardware.cpu.intel.updateMicrocode = true;
-  networking.hostName = "umaru";
-
-  nix.buildCores = 8;
+  boot.loader.grub.device = "/dev/vda";
+  networking.hostName = "tomoyo";
+  nix.buildCores = 2;
 }
