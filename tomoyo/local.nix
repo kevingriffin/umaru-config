23c290c6e3a07301910650cc9633f1d532e6c37d
Kevin Griffin <me@kevin.jp>
2018-11-11 11:37:59 +0000

    Experiment with socat

--- tomoyo/local.nix
+++ tomoyo/local.nix
@@ -23,7 +23,22 @@
   environment.systemPackages = with pkgs; [
      eikaiwa-packages
      seeing_is_believing
+     socat
   ];
+
+   systemd.services.socat-proxy = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "Start a proxy for Plex from iPv4 to iPv6";
+    serviceConfig = {
+      Type = "forking";
+      User = "root";
+      ExecStart = ''${pkgs.socat}/bin/socat -v OPENSSL-LISTEN:32400,fork,cert=/home/kevin/certs/wildcard.kevin.jp.pem,key=/home/kevin/certs/wildcard.kevin.jp.key,verify=0 TCP6:plex.kevin.jp:32400
+'';
+    };
+ };
+
+ networking.firewall.allowedTCPPorts = [ 32400 ];
 
   programs.mosh.enable = true;
 
