e3f33ac0b44a8498df8391632b8fd83538de3c96
Kevin Griffin <me@kevin.jp>
2018-12-27 05:01:12 +0000

    Umaru -> Erika

---
+++ erika/local.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../overlays.nix
+    ../eikaiwa-servers.nix
+   ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["zfs"];
+  boot.loader.grub.device = "nodev";
+
+
+  boot.initrd.luks.devices = [
+    {
+      name = "root1";
+      device = "/dev/disk/by-uuid/8537440e-66a3-4696-a9cc-69493e8e97f9";
+      allowDiscards = true;
+    }
+    {
+      name = "root2";
+      device = "/dev/disk/by-uuid/02be44d4-9def-47e7-95cb-413bd54130d0";
+      allowDiscards = true;
+    }
+  ];
+
+
+  boot.kernelParams = [ "nomodeset" ];
+
+  environment.systemPackages = with pkgs; [
+     eikaiwa-packages
+     seeing_is_believing
+  ];
+
+  hardware.cpu.intel.updateMicrocode = true;
+  networking.hostName = "erika";
+  networking.hostId = "a5621c46";
+
+  nix.buildCores = 8;
+}
