5dd538c73d293d01d491cade6700b81eaa444893
Kevin Griffin <me@kevin.jp>
2019-04-20 09:12:19 +0000

    Adds a bundler for development

--- overlays/packages/eikaiwa-environment.nix
+++ overlays/packages/eikaiwa-environment.nix
@@ -1,17 +1,18 @@
-{ lib, buildEnv, yarn, overmind, ruby_2_5, nodejs-10_x ? null }:
+{ lib, buildEnv, yarn, overmind, ruby_2_5, bundler, nodejs-10_x ? null }:
 
 let
   pkgsNewEnough = (lib.versionAtLeast yarn.version "1.10.0") && (nodejs-10_x != null);
 
   webpkgs = if pkgsNewEnough
-    then { inherit yarn nodejs-10_x; }
+    then { inherit yarn nodejs-10_x bundler; }
     else builtins.trace "Using <nixpkgs-unstable> channel for web packages"
          (import <nixpkgs-unstable> {});
 in
 
 let
-  nodejs = webpkgs.nodejs-10_x;
-  yarn   = webpkgs.yarn.override { inherit nodejs; };
+  nodejs  = webpkgs.nodejs-10_x;
+  yarn    = webpkgs.yarn.override { inherit nodejs; };
+  bundler = webpkgs.bundler;
 in
 
 buildEnv {
@@ -21,5 +22,6 @@ buildEnv {
     yarn
     overmind
     ruby_2_5
+    bundler
   ];
 }
