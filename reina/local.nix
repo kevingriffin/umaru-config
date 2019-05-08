8795806f6f15f524d1442416662770dae24ee0ee
Kevin Griffin <kev@bibo.com.ph>
2019-05-08 18:11:21 +0900

    Adds nginx for Reina.

--- reina/local.nix
+++ reina/local.nix
@@ -5,6 +5,21 @@
     ../modules/ruby-development.nix
   ];
 
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts."reina.local.kevin.jp" = {
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:3000";
+        proxyWebsockets = true;
+      };
+      sslCertificate = "/etc/nginx/letsencrypt/live/reina.local.kevin.jp/fullchain.pem";
+      sslCertificateKey = "/etc/nginx/letsencrypt/live/reina.local.kevin.jp/privkey.pem";
+    };
+  };
+
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
   nix.maxJobs = 4;
