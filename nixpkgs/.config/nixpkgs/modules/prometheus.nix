bec5a30a7bec7dcafec4aa01e59c1a5856fb351f
Kevin Griffin <me@kevin.jp>
2020-07-31 22:52:34 +0900

    tomoyo: pushover alerts and router monitoring

--- nixpkgs/.config/nixpkgs/modules/prometheus.nix
+++ nixpkgs/.config/nixpkgs/modules/prometheus.nix
@@ -1,7 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  services.prometheus = {
+  services.prometheus = let
+    secrets = import ../secrets.nix;
+  in
+  {
+    alertmanager = {
+      enable = true;
+      configText = ''
+        receivers:
+          - name: pushover
+            pushover_configs:
+              - user_key: ${secrets.pushover-user-key}
+                token: ${secrets.pushover-token}
+
+        route:
+          receiver: pushover
+          group_wait: 30s
+          group_interval: 5m
+          repeat_interval: 4h
+      '';
+    };
+
+    alertmanagers = [{
+      scheme = "http";
+      static_configs = [{
+        targets = [ "localhost:${toString config.services.prometheus.alertmanager.port}" ];
+      }];
+    }];
+
     enable = true;
     webExternalUrl = "https://tomoyo.kevin.jp/prometheus/";
     scrapeConfigs = [
@@ -22,11 +49,23 @@
             "erika.kevin.jp:9100"
             "umaru.kevin.jp:9100"
             "haru.kevin.jp:9100"
+            "pleinair.kevin.jp:9100"
+            "flonne.kevin.jp:9100"
           ];
         }];
       }
     ];
 
+    rules = [
+      ''
+        groups:
+          - name: down
+            rules:
+              - alert: ExporterDown
+                expr: up{transient!="yes"} == 0
+                for: 5m
+      ''
+    ];
   };
 }
 
