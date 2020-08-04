281ec627027f3e534b909ef5c46cce2996422291
Kevin Griffin <me@kevin.jp>
2020-08-04 20:51:29 +0900

    secrets-stub: new entries

--- nixpkgs/.config/nixpkgs/secrets-stub.nix
+++ nixpkgs/.config/nixpkgs/secrets-stub.nix
@@ -1,4 +1,5 @@
 {
+  borg-password                      = "";
   pushover-token                     = "";
   pushover-user-key                  = "";
   sesame-token                       = "";
@@ -6,4 +7,7 @@
   mosquitto-hass-ir-hashed-password  = "";
   mosquitto-adapter-hashed-password  = "";
   matrix-synapse-registration-secret = "";
+  znc-hash-password                  = "";
+  znc-salt                           = "";
+  znc-freenode-password              = "";
 }
