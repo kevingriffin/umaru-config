16cc0ed16b58470199b3614b5adbd86a36107d87
Kevin Griffin <me@kevin.jp>
2021-03-04 15:55:30 +0900

    packages: chef and cap no longer needed

--- nixpkgs/.config/nixpkgs/overlays/packages.nix
+++ nixpkgs/.config/nixpkgs/overlays/packages.nix
@@ -4,9 +4,6 @@ self: super: {
   iknow-devops = super.callPackage ./packages/iknow-devops.nix {};
   iknow-devops-legacy = super.callPackage ./packages/iknow-devops-legacy.nix {};
   phraseapp_updater = super.callPackage ./packages/phraseapp_updater {};
-  chef-11 = super.callPackage ./packages/chef-11 {};
-  chef-ctl = super.callPackage ./packages/chef-ctl {};
-  capistrano = super.callPackage ./packages/capistrano {};
   csshx = super.callPackage ./packages/csshx.nix {};
   cfn-lint = self.python2.pkgs.callPackage ./packages/cfn-lint.nix {};
   cfn_flip = self.python2.pkgs.callPackage ./packages/cfn_flip.nix {};
